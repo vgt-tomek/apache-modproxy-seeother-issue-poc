2790b7e55f42b281707104fa7e98f8486e2a1b2f
VGT <tomek@vgtworld.pl>
2014-06-02 20:34:31 +0200

    Display both links to redirect controllers instead of only fixed one.

--- seeother/src/main/java/pl/vgtworld/seeother/actions/Home.java
+++ seeother/src/main/java/pl/vgtworld/seeother/actions/Home.java
@@ -9,7 +9,8 @@ public class Home {
 	@GET
 	public String getHome() {
 		return "<h1>Home</h1>"
-			+ "<a href=\"redirect-fixed\">go to other</a>";
+			+ "<a href=\"redirect\">go to other</a>"
+			+ "| <a href=\"redirect-fixed\">fixed go to other</a>";
 	}
 	
 }
